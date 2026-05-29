781399670862a1016ceea307d53a79d1eace29de
osoTripolar <osotripolar@proton.me>
2026-05-29 06:01:40 -0500

    se dio funcionalidad a las activiades diarias, faltan terminar

--- src/static/js/dairy.js
+++ src/static/js/dairy.js
@@ -1,14 +1,55 @@
-// creamos un endpoint, lo guardamos como objeto, y lo modificamos, solo para lo visual de momento,
-// lo creamos manuelmente y el frontend debe de poder modificarlo y en cada modificacion hacer un put del objeto que se guardará como estado
-// tambien debemos manejar una logica para designar el dia, y que no se cruce la info
+// PENDIENTES
+
+// - crear botón finish day (para que marque como rojo todo lo que no se cumplió en el día)
+// - manejar la lógica del día (que reset() use una función para detectar el día y renovarlo, también poder modificarlo a mano)
+// - incluir comidas del día
+// - incluir notas diarias
+// - incluir actividades extraÑ
 
 // =========== ELEMENTOS HTML Y VARIABLES GLOBALES
 let dataState
 
 const btnResetDay = document.getElementById('resetDay')
+const btnPrintDatastate = document.getElementById('printDataState')
+const activities = document.querySelector('.activities-container')
 
 // =========== LISTENERS =================================
 btnResetDay.addEventListener('click', reset)
+
+activities.addEventListener('click', (e)=>{
+
+  const target = e.target
+
+  if (target.tagName != 'BUTTON') return 
+
+  const pather = target.closest('.activitie')
+  const id = pather.dataset.id
+
+  if (target.classList.contains('activitie__button--null')) setActivitieClass('null')
+  if (target.classList.contains('activitie__button--done')) setActivitieClass('done')
+  if (target.classList.contains('activitie__button--pending')) setActivitieClass('pending')
+  
+  function setActivitieClass(newClassName){
+    pather.className = `activitie activitie--${newClassName}`
+    dataState[id].status = newClassName
+  }
+  
+  try{
+    putActivities(dataState)
+  }catch(error){
+    console.error(error)
+  }
+
+})
+
+btnPrintDatastate.addEventListener('click',printDataState)
+
+// =========== FUNCIONES PROVISIONALES ====================
+
+function printDataState(){
+  console.log('imprimiendo data state')
+  console.log(dataState)
+}
 
 // =========== FUNCIONES =================================
 
@@ -19,6 +60,9 @@ async function init(){
 }
 
 async function reset(){
+
+  // realmente deberíamos abrir un modal de confirmación
+
   const data = await getTemplateDay()
   const resOk = await putActivities(data)
 
@@ -42,13 +86,47 @@ function renderAll(){
 }
 
 function paintData(data) {
-  data.forEach(element => {
+
+  const fragment = document.createDocumentFragment()
+  
+  // creamos un document fragment
+  data.forEach((element,index) => {
     // dependiendo del "type" del objeto crearemos un tipo de elemento html
     // BOOLEAN: tarea hecho o no
     // TEXT: para que este completa la tarea hay que detallar que se hizo
     // NOTES: basicamente un array con notas del dia (o periodo)
-    console.log(element)
+    
+    if(element.type == 'boolean'){
+      const xdd = newActivitie(index,element.name,element.status)
+      fragment.appendChild(xdd)
+    }
   })
+  
+  activities.replaceChildren(fragment)
+}
+
+function newActivitie(index,name,status){
+
+  const element = document.createElement('div')
+
+  element.className = `activitie activitie--${status}`
+  element.dataset.id = index
+
+  element.innerHTML = `
+    <p>${name}</p>
+    <div class="activitie__buttons">
+      <button class="activitie__button activitie__button--done">
+        <i class="bi bi-check"></i>
+      </button>
+      <button class="activitie__button activitie__button--null">
+        <i class="bi bi-chevron-bar-contract"></i>
+      </button>
+      <button class="activitie__button activitie__button--pending">
+        <i class="bi bi-x"></i>
+      </button>
+    </div>
+  `
+  return element
 }
 
 // =========== FUNCIONES FETCH ============================
